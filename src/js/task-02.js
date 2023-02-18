61beef36b43b25548b1e64c5d556127a70825f9f
Asselechi <104963856+Asselechi@users.noreply.github.com>
2023-02-19 00:04:23 +0600

    task 2

--- src/js/task-02.js
+++ src/js/task-02.js
@@ -6,3 +6,13 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+const newList = document.createElement('li');
+console.log(newList);
+
+newList.textContent = ingredients;
+newList.classList.add("item");
+
+const listIngredients = document.querySelector('ul#ingredients');
+console.log(listIngredients);
+listIngredients.append(newList);
